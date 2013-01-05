1b7618a40e4f2503cbb8eed8a248c3b8f4b6d6aa
Vedang Manerikar <ved.manerikar@gmail.com>
2013-01-05 21:34:11 +0530

    Add nrepl.el and ac-nrepl as submodules
    Also, create an nrepl-autoloads file for faster loading of nrepl

---
+++ lib/nrepl-autoloads.el
@@ -0,0 +1,49 @@
+;;; nrepl-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (nrepl nrepl-jack-in nrepl-disable-on-existing-clojure-buffers
+;;;;;;  nrepl-enable-on-existing-clojure-buffers nrepl-interaction-mode)
+;;;;;;  "nrepl/nrepl" "nrepl/nrepl.el" (20712 14314 0 0))
+;;; Generated autoloads from nrepl/nrepl.el
+
+(autoload 'nrepl-interaction-mode "nrepl/nrepl" "\
+Minor mode for nrepl interaction from a Clojure buffer.
+
+\\{nrepl-interaction-mode-map}
+
+\(fn &optional ARG)" t nil)
+
+(autoload 'nrepl-enable-on-existing-clojure-buffers "nrepl/nrepl" "\
+
+
+\(fn)" t nil)
+
+(autoload 'nrepl-disable-on-existing-clojure-buffers "nrepl/nrepl" "\
+
+
+\(fn)" t nil)
+
+(autoload 'nrepl-jack-in "nrepl/nrepl" "\
+
+
+\(fn &optional PROMPT-PROJECT)" t nil)
+
+(add-hook 'nrepl-connected-hook 'nrepl-enable-on-existing-clojure-buffers)
+
+(autoload 'nrepl "nrepl/nrepl" "\
+
+
+\(fn HOST PORT)" t nil)
+
+;;;***
+
+(provide 'nrepl-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; nrepl-autoloads.el ends here
