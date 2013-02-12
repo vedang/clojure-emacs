12b6b4a04ddabe6a637ab587196717fc0c1c1797
Vedang Manerikar <ved.manerikar@gmail.com>
2013-02-12 12:55:05 +0530

    Update clojure-mode and nrepl from upstream

--- lib/nrepl-autoloads.el
+++ lib/nrepl-autoloads.el
@@ -5,7 +5,7 @@
 
 ;;;### (autoloads (nrepl nrepl-jack-in nrepl-disable-on-existing-clojure-buffers
 ;;;;;;  nrepl-enable-on-existing-clojure-buffers nrepl-interaction-mode)
-;;;;;;  "nrepl/nrepl" "nrepl/nrepl.el" (20712 14314 0 0))
+;;;;;;  "nrepl/nrepl" "nrepl/nrepl.el" (20761 60957 0 0))
 ;;; Generated autoloads from nrepl/nrepl.el
 
 (autoload 'nrepl-interaction-mode "nrepl/nrepl" "\
@@ -16,26 +16,32 @@ Minor mode for nrepl interaction from a Clojure buffer.
 \(fn &optional ARG)" t nil)
 
 (autoload 'nrepl-enable-on-existing-clojure-buffers "nrepl/nrepl" "\
-
+Enable interaction mode on existing Clojure buffers.
+See command `nrepl-interaction-mode'.
 
 \(fn)" t nil)
 
 (autoload 'nrepl-disable-on-existing-clojure-buffers "nrepl/nrepl" "\
-
+Disable interaction mode on existing Clojure buffers.
+See command `nrepl-interaction-mode'.
 
 \(fn)" t nil)
 
 (autoload 'nrepl-jack-in "nrepl/nrepl" "\
-
+Start a nREPL server for the current project and connect to it.
+If PROMPT-PROJECT is t, then prompt for the project for which to
+start the server.
 
 \(fn &optional PROMPT-PROJECT)" t nil)
 
 (add-hook 'nrepl-connected-hook 'nrepl-enable-on-existing-clojure-buffers)
 
 (autoload 'nrepl "nrepl/nrepl" "\
-
+Connect nrepl to HOST and PORT.
 
 \(fn HOST PORT)" t nil)
+
+(eval-after-load 'clojure-mode '(progn (define-key clojure-mode-map (kbd "C-c M-j") 'nrepl-jack-in) (define-key clojure-mode-map (kbd "C-c M-c") 'nrepl)))
 
 ;;;***
 
